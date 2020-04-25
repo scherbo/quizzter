53cc873f44f0b2d0c6d03bcaa64e1aa5aaa69e4a
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-25 16:29:06 +0300

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { RootState } from '#root/store'
+import { Paragraph, Heading, FlexContainer, Link } from '#root/components'
 
 const Home = () => {
-  return <h3>Hello, it&apos;s home page</h3>
+  const isAuthenticated = useSelector<RootState, boolean>((state) => state.session.authenticated)
+  return (
+    <FlexContainer alignItems="center" justifyContent="center" css={{ minHeight: 500, textAlign: 'center' }}>
+      <div>
+        <Heading type="h2" css={{ marginBottom: 5 }}>
+          Welcome to the Quizzter!
+        </Heading>
+        <Paragraph size="md">
+          A place where you can build and solve quizes.{' '}
+          {isAuthenticated ? <Link href="/builder">Build a quiz!</Link> : <Link href="/auth">Sign in to start</Link>}
+        </Paragraph>
+      </div>
+    </FlexContainer>
+  )
 }
 
 export default Home
