f98745827fa7c89047046cb843f8664d525762a7
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-25 16:29:12 +0300

    update theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,16 +1,29 @@
 import styled, { CreateStyled } from '@emotion/styled'
 import { FONT_SIZE } from '#root/constants'
 
+/*
+  I decided not to use polished just yet, so each main color gonna have at least 3 shades
+  main colors:
+    - primary
+    - dark
+    - danger
+*/
 export const theme = {
   colors: {
     primary: '#3185FC',
+    primaryLighter: '#4a94fc',
+    primaryLightest: '#63a3fd',
+    dark: '#342e37',
+    darkLighter: '#413a45',
+    darkLightest: '#4e4553',
     danger: '#ff6464',
+    dangerLighter: '#ff7d7d',
+    dangerLightest: '#ff9797',
     pale: '#f5eaea',
     gray1: '#f8f8f8',
     gray2: '#dfdfdf',
     gray3: '#aaa',
     gray4: '#888',
-    dark: '#342e37',
     light: '#fafffd',
   },
   fonts: {
