367fd2158368fb7981e08def2f27f07c1f66ba5d
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-25 16:27:45 +0300

    update header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
 
-import { Navigation } from '#root/components/Navigation'
+import styled from '#root/theme'
+import { FlexContainer, Navigation } from '#root/components'
 
-export const Header = () => <Navigation />
+export const Logo = styled.a`
+  text-decoration: none;
+  font-size: ${({ theme }) => theme.fonts.bigTextSize};
+  color: ${({ theme }) => theme.colors.dark};
+  cursor: pointer;
+`
+
+export const HeaderTag = styled.header`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray2};
+  display: flex;
+  align-items: center;
+`
+
+export const Header = () => (
+  <HeaderTag>
+    <FlexContainer alignItems="center" justifyContent="space-between">
+      <Link href="/">
+        <Logo>Quizzter</Logo>
+      </Link>
+      <Navigation />
+    </FlexContainer>
+  </HeaderTag>
+)
