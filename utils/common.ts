26dbc5370bee49c1dbc19c279fee201b7be02ee0
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-26 20:16:56 +0300

    has error util

--- utils/common.ts
+++ utils/common.ts
@@ -1,1 +1,7 @@
 export const isServer = () => typeof window === 'undefined'
+
+export const hasError = (meta: Record<string, any> = {}) => {
+  const { submitError, dirtySinceLastSubmit, error, touched } = meta
+
+  return (error || (submitError && !dirtySinceLastSubmit)) && touched
+}
