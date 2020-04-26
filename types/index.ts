93ab3ab1e950bbcba770148109957fe41889d74d
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-26 20:16:48 +0300

    add auth forms state types

--- types/index.ts
+++ types/index.ts
@@ -3,4 +3,17 @@ import { NextPageContext } from 'next'
 import { Store } from '@reduxjs/toolkit'
 
 export type MyAppProps = AppProps & { store: Store }
-export type ExtendedAppContext = AppContext & { ctx: NextPageContext & { store: Store } }
+export type ExtendedNextPageContext = NextPageContext & { store: Store }
+export type ExtendedAppContext = AppContext & { ctx: ExtendedNextPageContext }
+
+export type SigninState = {
+  email: string
+  password: string
+}
+
+export type SignupState = {
+  name: string
+  email: string
+  password: string
+  passwordRepeat: string
+}
