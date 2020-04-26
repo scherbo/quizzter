a1bbb73dfff2463cfe44859419321d371253b030
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-26 20:14:43 +0300

    add gap prop to grid layout

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -7,12 +7,20 @@ const position = {
   stretch: 'stretch',
 }
 
+const gapSizes = {
+  sm: '14px',
+  md: '18px',
+  lg: '24px',
+  xl: '30px',
+}
+
 type GridLayoutProps = {
   inline?: boolean
   justifyItems?: keyof typeof position
   alignItems?: keyof typeof position
   templateColumns?: string
   templateRows?: string
+  gap?: keyof typeof gapSizes
 }
 
 const GridLayout = styled.div<GridLayoutProps>`
@@ -21,6 +29,7 @@ const GridLayout = styled.div<GridLayoutProps>`
   align-items: ${({ alignItems }) => alignItems};
   grid-template-columns: ${({ templateColumns }) => templateColumns};
   grid-template-rows: ${({ templateRows }) => templateRows};
+  gap: ${({ gap }) => gap && gapSizes[gap]};
 `
 
 const GridBox = styled.div``
