67c1165590117a39cc0ffa5932e8d87393e73ef6
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-04-30 16:50:23 +0300

    link as prop

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -27,13 +27,14 @@ const ATag = styled.a`
 `
 
 type LinkProps = {
+  as?: string
   href: string
   prefetch?: boolean
   children: React.ReactChild | React.ReactChild[]
 }
 
-export const Link = ({ href, prefetch, children }: LinkProps) => (
-  <NextLink href={href} prefetch={prefetch}>
+export const Link = ({ href, as, prefetch, children }: LinkProps) => (
+  <NextLink href={href} as={as} prefetch={prefetch}>
     <ATag>{children}</ATag>
   </NextLink>
 )
