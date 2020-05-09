1964f0b788a2540e188ad5013a9d188ade0ce718
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-05-09 22:21:26 +0300

    update components so use only theme colors

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,12 +7,12 @@ import { Container, Navigation } from '#root/components'
 export const Logo = styled.a`
   text-decoration: none;
   font-size: ${({ theme }) => theme.fonts.bigTextSize};
-  color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.colors.textMain};
   cursor: pointer;
 `
 
 export const HeaderTag = styled.header`
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray2};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderSecondary};
   display: flex;
   align-items: center;
 `
