95ca90b3cc5a4c5d07983e8ebcf4b78abb4a0c9c
Sergey Scherbo <thomaskiniaev@gmail.com>
2020-05-23 21:27:41 +0300

    update navigation with theme toggler

--- components/Layout/components/Header/components/Navigation/Navigation.tsx
+++ components/Layout/components/Header/components/Navigation/Navigation.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import styled from '#root/theme'
+import { useSelector, useDispatch } from 'react-redux'
+import styled, { LIGHT, DARK } from '#root/theme'
 
-import { RootState } from '#root/store'
+import { RootState, changeTheme } from '#root/store'
 import { setCookies } from '#root/utils'
 
 import { NavLink, ATag } from './NavLink'
+
+export const NavTag = styled.nav`
+  display: flex;
+  align-items: center;
+`
 
 export const NavList = styled.ul`
   display: flex;
 `
 
 export const NavListItem = styled.li`
+  &:not(:last-child) {
+    margin-right: 15px;
+  }
+`
+
+const ThemeColor = styled.div`
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => {
+    return theme.name === 'lightTheme' ? DARK : LIGHT
+  }};
+
   &:not(:last-child) {
     margin-right: 15px;
   }
@@ -45,14 +63,21 @@ const AUTHENTICATED_NAVIGATION = [
 
 export const Navigation = () => {
   const authenticated = useSelector<RootState, boolean>((s) => s.session.authenticated)
+  const theme = useSelector<RootState, string>((s) => s.theme)
+  const dispatch = useDispatch()
 
   const handleLogout = () => {
     setCookies({ key: 'token', value: null })
     location.assign('/')
   }
 
+  const handleChangeTheme = () => {
+    changeTheme(dispatch, theme === 'light' ? 'dark' : 'light')
+  }
+
   return (
-    <nav>
+    <NavTag>
+      <ThemeColor onClick={handleChangeTheme} />
       <NavList>
         {(authenticated ? AUTHENTICATED_NAVIGATION : NAVIGATION).map(({ text, href }) => (
           <NavListItem key={text}>
@@ -65,6 +90,6 @@ export const Navigation = () => {
           </ATag>
         )}
       </NavList>
-    </nav>
+    </NavTag>
   )
 }
