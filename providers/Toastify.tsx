0dbe0f562b15aa33bf5b90bd5d71ca0bf7ca1fae
Jason <kiniaevjason@gmail.com>
2020-11-29 18:23:28 +0300

    bare-bones toastify setup

---
+++ providers/Toastify.tsx
@@ -0,0 +1,32 @@
+import { ToastContainer as NotStyledContainer } from 'react-toastify'
+import styled from '@emotion/styled'
+import { useTheme } from 'emotion-theming'
+
+import { Theme } from '#root/theme'
+
+export const StyledToasts = styled(NotStyledContainer)<{ success: string; danger: string }>`
+  .Toastify__toast {
+    font-size: 14px;
+  }
+
+  .Toastify__toast--success {
+    background-color: ${({ success }) => success};
+  }
+
+  .Toastify__toast--error {
+    background-color: ${({ danger }) => danger};
+  }
+`
+
+export const ToastContainer = () => {
+  const theme = useTheme<Theme>()
+  return (
+    <StyledToasts
+      position="bottom-right"
+      autoClose={3500}
+      hideProgressBar
+      success={theme.colors.primary}
+      danger={theme.colors.danger}
+    />
+  )
+}
